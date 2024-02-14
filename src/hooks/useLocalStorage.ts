d9792edffce25f07ab13382330fab9d6840ce16a
Rasto <rastouhrin1@gmail.com>
2024-02-14 16:28:52 +0100

    .

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -14,6 +14,7 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
 //     } else {
 //       return initialValue
 //     }
+//     }
 //   })
    const [value, setValue] = useState([] as number[]);
   useEffect(() => {
