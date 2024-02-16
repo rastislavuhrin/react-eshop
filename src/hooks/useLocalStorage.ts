731b8f4e112d0a848559ee8b4dafe7936679b8d4
Rasto <rastouhrin1@gmail.com>
2024-02-16 20:58:02 +0100

    cas

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -14,7 +14,7 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
 //     } else {
 //       return initialValue
 //     }
-//     }hs
+//     }hsnemam cas
 //   })
    const [value, setValue] = useState([] as number[]);
   useEffect(() => {
