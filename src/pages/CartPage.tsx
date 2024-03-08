ba3a65804ca019fd74dd04ba54801086e66f1484
Rasto <rastouhrin1@gmail.com>
2024-03-08 08:07:38 +0100

    13

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,7 +3,7 @@ import { CartContext } from '../context/CartContextProvider';
 import { Product } from '../Components/Product';
 import Link from 'next/link';
 import { CartItem } from '../Components/CartItem';
-// CartPage 12
+// CartPage 13
 const CartPage = () => {
    const { addToCard, cardItems, removeFromCart } =
       React.useContext(CartContext);
