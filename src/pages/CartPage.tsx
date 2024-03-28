3f530c3b2ea40adff078c1c924e54b04845a08f8
Rasto <rastouhrin1@gmail.com>
2024-03-28 18:29:12 +0100

    Refactored calculate_area function

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,7 +3,7 @@ import { CartContext } from '../context/CartContextProvider';
 import { Product } from '../Components/Product';
 import Link from 'next/link';
 import { CartItem } from '../Components/CartItem';
-// CartPage 35
+// CartPage 36
 5;
 // "Refactored calculate_area function for clarity and improved efficiency. "
 
